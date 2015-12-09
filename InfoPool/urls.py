1647f4f81f9272569388f990490c7dc614fe638f
Gonzalo Tittarelli <gonzalotittarelli@hotmail.com>
2015-12-08 23:58:24 -0300

    template index

--- InfoPool/urls.py
+++ InfoPool/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^infopool/', include('infopool.urls')),
+    url(r'^infopool/admin/', include(admin.site.urls)),
 ]
